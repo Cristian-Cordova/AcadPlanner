fb25da327a6771013dbd97b6777dad268587db59
Cristian Cordova <cristiancordova177@gmail.com>
2026-06-03 04:36:40 -0600

    Initialize Firebase in app entry point

--- AcadPlanner/AcadPlannerApp.swift
+++ AcadPlanner/AcadPlannerApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AcadPlannerApp: App {
+    init()
+    {
+        configureFirebase()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func configureFirebase() {
+        guard FirebaseApp.app() == nil,
+              Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil
+        else {
+            return
+        }
+        
+        FirebaseApp.configure()
     }
 }
